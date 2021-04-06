e5cdd95c60fdc2a19de958fc5a552d27dda7f8a0
00008949 <00008949wiut@gmail.com>
2021-04-06 19:56:28 +0500

    added all files

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'pug')
+app.use('/static', express.static('./public'))
 app.get('/', (req, res) => {
-res.render('Home')
+res.render('index')
+})
+
+app.get('/create', (req,res) => {
+    res.render('create')
+})
+
+app.get('/posts/detail', (req, res) => {
+    res.render('detail')
 })
 
 app.listen(8000, err=> {
