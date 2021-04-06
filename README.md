0eb9500ae0b99054a4bf8d69f9b5fbffd5ea8d3c
00008949 <00008949wiut@gmail.com>
2021-04-06 23:43:34 +0500

    Final version

---
+++ README.md
@@ -0,0 +1,34 @@
+# Blog web application
+
+### Blog web application is designed to help users to post their blogs.
+
+To run this applicitaion locally you must follow the steps indicated below:
+
+1. Clone the repository where the file is located
+```bash
+git clone https://github.com/00008949/WebTechCW2sem2.git
+```
+2. Install all of the indicated files and unzip them
+```bash
+cd blog-app
+npm install
+```
+
+3. Run the application
+``bash
+node app
+```
+4. To exit the app type
+
+```
+ctrl+C
+```
+
+### Web Application Dependencies:
+-express.js https://expressjs.com/ru/
+-pug.js https://pugjs.org/api/getting-started.html
+
+### The link to the web application repository
+
+1. https://github.com/00008949/WebTechCW2sem2.git
+
--- app.js
+++ app.js
@@ -1,21 +1,117 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
-app.set('view engine', 'pug')
+let postsDb = []
+
+fs.readFile('./data/posts.json', (err, data) => {
+	if (!err) {
+		postsDb = JSON.parse(data)
+	}
+})
+
+const parser = require('body-parser')
+
+
 app.use('/static', express.static('./public'))
+app.use(express.urlencoded({ extended: false}))
+app.set('view engine', 'pug')
+
+
+
 app.get('/', (req, res) => {
-res.render('index')
+    res.render('index')
 })
 
-app.get('/create', (req,res) => {
-    res.render('create')
+app.get('/create', (req, res) => {
+	res.render('create')
 })
 
-app.get('/posts/detail', (req, res) => {
-    res.render('detail')
+app.post('/create', (req,res) => {
+    const title = req.body.title
+    const description = req.body.description
+
+    if (title.trim() === '' && description.trim() === ''){
+
+    res.render('create', { error: true})}
+    else {
+        fs.readFile('./data/posts.json', (err, data) => {
+            if (err) throw err
+
+            const posts = JSON.parse(data)
+
+            posts.push({
+                id: id (),
+                title: title,
+                description: description,
+
+            })
+
+            fs.writeFile('./data/posts.json', JSON.stringify(posts), err => {
+                if (err) throw err
+
+                res.render('create', { success:true})
+            })
+
+        })
+    }
 })
+
+
+
+app.get('/posts', (req, res) =>{
+
+    fs.readFile('./data/posts.json', (err, data) => {
+        if (err) throw err
+
+        const posts = JSON.parse(data)
+    
+    res.render('posts', { posts: posts})
+    })
+})
+
+
+app.get('/posts/:id', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/posts.json', (err, data) => {
+        if (err) throw err
+
+        const posts = JSON.parse(data)
+
+        const post = posts.filter(post => post.id == id)[0]
+    
+    res.render('detail', { posts: post})
+    })
+
+})
+
+
+
+app.post('/create', (req, res) => {
+
+	const post = {
+		id: generateRandomId(),
+		title: req.body.title,
+		body: req.body.details
+    }
+
+    postsDb.push(post)
+	fs.writeFile('./data/posts.json', JSON.stringify(postsDb), (err) => {
+		if (err) {
+			res.redirect('/create?success=0')
+		} else {
+			res.redirect('/create?success=1')
+		}
+	})
+})
+    
 
 app.listen(8000, err=> {
     if(err) console.log(err)
     console.log('Server is running on port 8000...')
 })
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
